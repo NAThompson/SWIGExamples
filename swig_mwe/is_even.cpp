6d58a8d9907224b37acfcca6a548bbea03f546ac
Nicholas <nthompson@slb.com>
2015-05-19 16:23:43 -0500

    MWE for SWIG.

---
+++ swig_mwe/is_even.cpp
@@ -0,0 +1,11 @@
+#include "is_even.hpp"
+
+bool is_even(int n)
+{
+    if(n % 2 == 0)
+    {
+      return true;
+    }
+
+    return false;
+}
---
+++ swig_mwe/is_even.i
@@ -0,0 +1,6 @@
+%module is_even
+%{
+#include "is_even.hpp"
+%}
+
+bool is_even(int n);
